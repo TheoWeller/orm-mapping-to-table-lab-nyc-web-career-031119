51530bfbd21f3895af4afb81f23341b6cf0d6a1e
Theo Weller <weller.theodor@gmail.com>
2019-03-21 18:19:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ DB[:conn].execute(sql, self.name, self.grade)
 end
 
 def self.create(name:, grade:)
-student = Students.new(name, grade)
+student = Student.new(name, grade)
 student.save
 student
 end
