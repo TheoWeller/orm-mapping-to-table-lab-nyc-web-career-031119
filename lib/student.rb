430b10e110ea87b425513305fef12f64a332bf00
Theo Weller <weller.theodor@gmail.com>
2019-03-21 18:07:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,13 @@ SQL
 DB[:conn].execute(sql)
 end
 
+def save
+sql = <<-SQL
+
+SQL
+DB[:conn].execute(sql)
+end
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
